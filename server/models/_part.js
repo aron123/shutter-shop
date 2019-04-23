74d462c8ba1902111ddc1e2999968467e94fb1e2
Kiss, Aron <aron123dev@gmail.com>
2019-04-23 13:46:42 +0200

    Fix Part model to validate input

--- server/models/_part.js
+++ server/models/_part.js
@@ -1,7 +1,22 @@
+const ModelValidationError = require('./errors/ModelValidationError');
+
 class Part {
     constructor (part) {
+
+        validatePart(part);
+
         this.pieces = Number(part.pieces);
         this.name = String(part.name);
+    }
+}
+
+function validatePart (part) {
+    if (!part.pieces) {
+        throw new ModelValidationError(`No pieces count is given or it is 0 for part, got: ${part.pieces}`);
+    }
+
+    if (!part.name) {
+        throw new ModelValidationError(`No name is given for part, got: ${part.name}`);
     }
 }
 
