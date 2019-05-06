a8447641eb4ef66be9127a65dc35de6da146f9cb
Kiss, Aron <aron123dev@gmail.com>
2019-05-06 17:59:28 +0200

    Add database initializer instead of mongodump

---
+++ initial-database/init-db.sh
@@ -0,0 +1,14 @@
+# For presenting assignment in webtech2 course.
+
+MONGO_HOST_IP=127.0.0.1
+MONGO_PORT=27017
+DATABASE_NAME=shutterShop
+
+docker run --detach -p $MONGO_PORT:$MONGO_PORT mongo
+
+mongoimport --host $MONGO_HOST_IP --db $DATABASE_NAME --collection employees employees.json
+mongoimport --host $MONGO_HOST_IP --db $DATABASE_NAME --collection customers customers.json
+mongoimport --host $MONGO_HOST_IP --db $DATABASE_NAME --collection shutters shutters.json
+mongoimport --host $MONGO_HOST_IP --db $DATABASE_NAME --collection orders orders.json
+
+mongo --host $MONGO_HOST_IP
