f7884d9df4a7012f0449cabeb46194e095791315
Kiss, Aron <aron123dev@gmail.com>
2019-05-13 12:05:11 +0200

    Complete db init script
    Add #!/bin/bash header, make sure that json files have read permission

--- initial-database/init-db.sh
+++ initial-database/init-db.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # For presenting assignment in webtech2 course.
 
 MONGO_HOST_IP=127.0.0.1
@@ -5,6 +7,8 @@ MONGO_PORT=27017
 DATABASE_NAME=shutterShop
 
 docker run --detach -p $MONGO_PORT:$MONGO_PORT mongo
+
+chmod +r *.json
 
 mongoimport --host $MONGO_HOST_IP --db $DATABASE_NAME --collection employees employees.json
 mongoimport --host $MONGO_HOST_IP --db $DATABASE_NAME --collection customers customers.json
